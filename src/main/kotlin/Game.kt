2480bbde18ad6d9d1b6145ca95445ef61d73a002
jessicalopesvs <37242604+jessicalopesvs@users.noreply.github.com>
2023-11-08 14:53:16 +0000

    Initial commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,7 @@
+class Game {
+    var titlePane = ""
+    // var you can modify the value
+    var image = ""
+    //    val = you can't modify just if you change the var
+    val description = ""
+}
